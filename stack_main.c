693bfd591b2b1548fffb8553f1996e49b8240613
varsha2097 <54901277+varsha2097@users.noreply.github.com>
2019-10-05 16:13:37 +0530

    Add files via upload
    Check balancing of symbols ( like ‘{‘, ‘(‘, ‘[‘ ) and their order using stack.
    Evaluate postfix expression using stack.  Example: postfix fix expression is 6 5 2 3 + 8 * + 3 + *

---
+++ stack_main.c
@@ -0,0 +1,78 @@
+#include "stack.h"
+#include<stdint.h>
+#include<assert.h>
+#include<stddef.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+int balancing_symbols(char c[10])
+{
+
+	Stack s = stack_new(5);
+	Stack *stk = &s;
+
+	Stack_Result res;
+
+	for(int i=0;i<strlen(c);i++)
+	{
+
+		if(c[i] == '[' || c[i] == '(' || c[i] == '{')
+		{
+			stk = stack_push(stk,c[i],&res);
+			continue;
+		}
+		else if(c[i] == ']' && stk->data[stk->top]=='[')
+		{
+			stk = stack_pop(stk,&res);
+			continue;
+		}
+		else if(c[i] == ')' && stk->data[stk->top]=='(')
+		{
+			stk = stack_pop(stk,&res);
+			continue;
+		}
+		else if(c[i] == '}' && stk->data[stk->top]=='{')
+		{
+			stk = stack_pop(stk,&res);
+			continue;
+		}
+		else
+		{
+			assert(stk->top==2 || stk->top == -1);
+			return 0;
+		}
+
+	}
+	stk = stack_peek(stk,&res);
+	assert(stk->top == -1);
+
+	return 0;
+}
+
+int postfix()
+{
+	Stack s = stack_new(5);
+	Stack *stk = &s;
+
+	Stack_Result res;
+	for(int i=0;i<strlen(c);i++)
+	{
+	char c[15]= "6523+8*+3+*";
+
+}
+
+}
+int main()
+{
+	char c[10] = "}((]";
+	char c1[20] = "[({})]";
+
+
+	balancing_symbols(c1);
+
+	postfix();
+
+	return 0;
+
+}
